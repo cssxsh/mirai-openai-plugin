86e195fb9a5bf4d5f2f03815c4e7c57ad99c6027
cssxsh <cssxsh@gmail.com>
2023-03-02 22:29:47 +0800

    fix: finishReason close #11

--- src/main/kotlin/xyz/cssxsh/openai/Choice.kt
+++ src/main/kotlin/xyz/cssxsh/openai/Choice.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.*
 @Serializable
 public data class Choice(
     @SerialName("finish_reason")
-    val finishReason: String = "",
+    val finishReason: String? = null,
     @SerialName("index")
     val index: Int = 0,
     @SerialName("logprobs")
