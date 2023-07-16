ef34183e8b8d61933454728a377dbb763aba4cad
cssxsh <cssxsh@gmail.com>
2023-07-16 20:20:58 +0800

    update: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.serialization") version "1.7.22"
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
 
-    id("net.mamoe.mirai-console") version "2.14.0"
+    id("net.mamoe.mirai-console") version "2.15.0"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
@@ -29,12 +29,12 @@ dependencies {
     compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.7.1")
     testImplementation(kotlin("test"))
     //
-    implementation(platform("net.mamoe:mirai-bom:2.14.0"))
+    implementation(platform("net.mamoe:mirai-bom:2.15.0"))
     compileOnly("net.mamoe:mirai-console-compiler-common")
     testImplementation("net.mamoe:mirai-core-mock")
     testImplementation("net.mamoe:mirai-logging-slf4j")
     //
-    implementation(platform("io.ktor:ktor-bom:2.1.3"))
+    implementation(platform("io.ktor:ktor-bom:2.2.4"))
     implementation("io.ktor:ktor-client-okhttp")
     implementation("io.ktor:ktor-client-auth")
     implementation("io.ktor:ktor-client-encoding")
@@ -44,7 +44,7 @@ dependencies {
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
     implementation("com.squareup.okhttp3:okhttp-dnsoverhttps")
     //
-    implementation(platform("org.slf4j:slf4j-parent:2.0.6"))
+    implementation(platform("org.slf4j:slf4j-parent:2.0.7"))
     testImplementation("org.slf4j:slf4j-simple")
 }
 
